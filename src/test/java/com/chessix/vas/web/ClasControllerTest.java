f282658c29ada3b960818c26f8af9b7ea44deab8
Mark Wigmans <mark.wigmans@gmail.com>
2014-07-03 15:10:31 +0200

    first attempt to make Spring MVC integration tests work

---
+++ src/test/java/com/chessix/vas/web/ClasControllerTest.java
@@ -0,0 +1,44 @@
+package com.chessix.vas.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.chessix.vas.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = Application.class)
+@IntegrationTest
+public class ClasControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    @Ignore("embedded redis database is needed")
+    public void testClean() throws Exception {
+        final String clasId = "123";
+        mockMvc.perform(post("/clas/" + clasId + "/clean")).andExpect(status().isOk());
+    }
+
+}
